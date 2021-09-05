9e9c2f53e03da4870f5a7cf7ff66804c4d869f4e
Selfie SD <83945969+Selfie-bd@users.noreply.github.com>
2021-09-05 11:14:32 +0530

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -72,7 +72,7 @@ class Uploader:
                 + "\nUploaded to YouTube with https://tx.me/youtubeitbot"
             )[:5000]
             if not Config.UPLOAD_MODE:
-                privacyStatus = "private"
+                privacyStatus = "public"
             else:
                 privacyStatus = Config.UPLOAD_MODE
 
